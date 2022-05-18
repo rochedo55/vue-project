d3ef734adf576c44d007199e0b340746e65edda7
gabrielSouza55 <rochasouzagabriel55@gmail.com>
2022-05-18 14:19:49 -0300

    Arquivos para storybook e implementacaod o tab em vue

---
+++ src/components/TabContent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="contents">
+    <button className="btn btn-primary" @click.prevent="$emit('onChange', index)">
+      {{ text }}
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  index: string[];
+  text: string[];
+}>();
+defineEmits<{(e: "onChange", val: number): void}>();
+</script>
---
+++ src/components/Tabs.vue
@@ -0,0 +1,37 @@
+<template>
+  <div className="p-3 mb-2 bg-light text-dark container">
+    <h1 className="white-text">TabComponent</h1>
+
+    <div class="tabsPane">
+      <div className="p-3 mb-2 bg-light text-dark">
+        <TabContent
+          v-for="tabButton, i in tabButtons"
+          :key="i"
+          :index="i"
+          :text="tabButton"
+          @onChange="(val) => updateContent(val)"
+        />
+      </div>
+    </div>
+  </div>
+  <div>
+    {{ content[i] }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import TabContent from "./TabContent.vue";
+
+
+    function updateContent(val: number) {
+        console.log("mudou")
+        i.value = val;
+    }
+    defineProps<{
+    tabButtons: string[];
+    content: string[];
+    }>();
+    const i = ref(0);
+
+</script>
